2b4c77a5341de50414f4bc5e73cd2ca443f549bc
Rodrigo Cardoso Buske <rodbuske@gmail.com>
2024-09-24 00:49:14 -0300

    feat: Add move functions

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+DIR=~/.dotfiles
+
 install_brew() {
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
@@ -8,9 +10,20 @@ install_oh_my_zsh() {
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
+move_zsh() {
+    ln -fs ${DIR}/zsh/zshrc ~/.zshrc
+    ln -fs ${DIR}/zsh/zprofile ~/.zprofile
+}
+
+move_gitconfig() {
+    ln -fs ${DIR}/gitconfig ~/.gitconfig
+}
+
 install() {
     # install_brew
-    install_oh_my_zsh
+    # install_oh_my_zsh
+    # move_zsh
+    move_gitconfig
 }
 
 install
